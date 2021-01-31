ed3356f429e0cb5ab7266bae6eb625cd96f7b4db
Frizio03 <fabrizio.tedeschi.scuola@gmail.com>
2021-01-31 12:06:18 +0100

    Svolti esercizi node-fetch 3-4-5

--- node-fetch/esercizio.js
+++ node-fetch/esercizio.js
@@ -9,7 +9,7 @@ function Es1 (){
     fetch(linkapi)
         .then(res => res.json())
         .then(data => {
-            lista = [];
+            lista = ["Es. 1"];
             data
                 .filter(e => e.userId === 4)
                 .forEach(e => lista.push(e.id))
@@ -22,7 +22,7 @@ function Es2 (){
     fetch(linkapi)
         .then(res => res.json())
         .then(data => {
-            lista2 = [];
+            lista2 = ["Es. 2"];
             data
                 .filter(e => e.userId%2 === 0)
                 .forEach(e => lista2.push(e.id))
@@ -35,12 +35,62 @@ function Es3 (){
     fetch(linkapi)
         .then(res => res.json())
         .then(data => {
+            lista3 = ["Es. 3"];
+            let totaleParole
             data
-                .filter(e => e.title%2 === 0)
-                .forEach(e => console.log(e.id))
+                .forEach(e => {
+                    totaleParole = 1
+                    for(let i=0; i<e.title.length; i++){
+                        if(e.title[i] === " ")
+                            totaleParole +=1
+                    }
+                    if(totaleParole === 7)
+                        lista3.push(e.id)
+                })
+            console.log(lista3)
+        })
+}
+
+function eliminaSpazi(stringa){
+    for(let i; i<stringa.leght; i++){
+        if (stringa[i]===" ")
+            stringa[i] = ""
+    }
+    return stringa
+}
+
+function Es4 (){
+    //Stampare sul terminale l'id di tutti i post con un numero di lettere nel body che sia multiplo di 3
+    fetch(linkapi)
+        .then(res => res.json())
+        .then(data => {
+            lista4 = ["Es. 4"];
+            data
+                .forEach(e => {
+                    e.body = eliminaSpazi(e.body)
+                })
+            data
+                .filter(e => e.body.leght%3 === 0)
+                .forEach(e => lista4.push(e.id))
+            console.log(lista4)
+        })
+}
+
+function Es5 (){
+    //Stampare sul terminale un’unica stringa che sia formata da tutti i body di tutti i post il cui titolo inizia per “s”
+    fetch(linkapi)
+        .then(res => res.json())
+        .then(data => {
+            string = "Es. 5: "
+            data
+                .filter(e => e.title[0] === "s")
+                .forEach(e => string += e.body)
+            console.log(string)
         })
 }
 
 Es1()
 Es2()
-//Es3()
+Es3()
+Es4()
+Es5()
